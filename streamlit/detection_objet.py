765a9d01f67533a62b55cc98060b097b6fa34a5f
Thibault Prouteau <thibault.prouteau.etu@univ-lemans.fr>
2021-06-03 10:37:19 +0200

    Add TODOs for Valentin.

--- streamlit/detection_objet.py
+++ streamlit/detection_objet.py
@@ -40,49 +40,6 @@ logger = logging.getLogger(__name__)
 
 
 # This code is based on https://github.com/streamlit/demo-self-driving/blob/230245391f2dda0cb464008195a470751c01770b/streamlit_app.py#L48  # noqa: E501
-def download_file(url, download_to: Path, expected_size=None):
-    # Don't download the file twice.
-    # (If possible, verify the download using the file length.)
-    if download_to.exists():
-        if expected_size:
-            if download_to.stat().st_size == expected_size:
-                return
-        else:
-            st.info(f"{url} is already downloaded.")
-            if not st.button("Download again?"):
-                return
-
-    download_to.parent.mkdir(parents=True, exist_ok=True)
-
-    # These are handles to two visual elements to animate.
-    weights_warning, progress_bar = None, None
-    try:
-        weights_warning = st.warning("Downloading %s..." % url)
-        progress_bar = st.progress(0)
-        with open(download_to, "wb") as output_file:
-            with urllib.request.urlopen(url) as response:
-                length = int(response.info()["Content-Length"])
-                counter = 0.0
-                MEGABYTES = 2.0 ** 20.0
-                while True:
-                    data = response.read(8192)
-                    if not data:
-                        break
-                    counter += len(data)
-                    output_file.write(data)
-
-                    # We perform animation by overwriting the elements.
-                    weights_warning.warning(
-                        "Downloading %s... (%6.2f/%6.2f MB)"
-                        % (url, counter / MEGABYTES, length / MEGABYTES)
-                    )
-                    progress_bar.progress(min(counter / length, 1.0))
-    # Finally, we remove these visual elements by calling .empty().
-    finally:
-        if weights_warning is not None:
-            weights_warning.empty()
-        if progress_bar is not None:
-            progress_bar.empty()
 
 
 WEBRTC_CLIENT_SETTINGS = ClientSettings(
@@ -110,34 +67,7 @@ def app_object_detection():
     This model and code are based on
     https://github.com/robmarkcole/object-detection-app
     """
-    #MODEL_URL = "https://github.com/robmarkcole/object-detection-app/raw/master/model/MobileNetSSD_deploy.caffemodel"  # noqa: E501
-    #MODEL_LOCAL_PATH = HERE / "./models/MobileNetSSD_deploy.caffemodel"
-    #PROTOTXT_URL = "https://github.com/robmarkcole/object-detection-app/raw/master/model/MobileNetSSD_deploy.prototxt.txt"  # noqa: E501
-    #PROTOTXT_LOCAL_PATH = HERE / "./models/MobileNetSSD_deploy.prototxt.txt"
 
-#    CLASSES = [
-#        "background",
-#        "aeroplane",
-#        "bicycle",
-#        "bird",
-#        "boat",
-#        "bottle",
-#        "bus",
-#        "car",
-#        "cat",
-#        "chair",
-#        "cow",
-#        "diningtable",
-#        "dog",
-#        "horse",
-#        "motorbike",
-#        "person",
-#        "pottedplant",
-#        "sheep",
-#        "sofa",
-#        "train",
-#        "tvmonitor",
-#    ]
     COCO_CATEGORY_NAMES = [
     '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
     'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
@@ -155,8 +85,6 @@ def app_object_detection():
 
     COLORS = np.random.uniform(0, 255, size=(len(COCO_CATEGORY_NAMES), 3))
 
-#    download_file(MODEL_URL, MODEL_LOCAL_PATH, expected_size=23147564)
-#    download_file(PROTOTXT_URL, PROTOTXT_LOCAL_PATH, expected_size=29353)
 
     DEFAULT_CONFIDENCE_THRESHOLD = 0.5
 
@@ -193,8 +121,8 @@ def app_object_detection():
                 box = box.detach().cpu().numpy()
                 draw.rectangle(box, outline='black')
                 label_str =  category_names[label.cpu().numpy()] if category_names else str(label.cpu().numpy()+2)
-                draw.text((box[0], box[1]), label_str, fill=(0,128,256,256), font=font)
-                result.append(Detection(name=label_str, prob=float(0.2)))
+                draw.text((box[0], box[1]), label_str, fill=(0,128,256,256), font=font) ## TODO: Passer la valeur de confiance de la prédiction.
+                result.append(Detection(name=label_str, prob=float(0.2))) ## TODO: prob doit prendre la valeur de confiance de la prédiction.
             return im, result
 
         def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
@@ -267,6 +195,6 @@ if __name__ == "__main__":
     st_webrtc_logger.setLevel(logging.DEBUG)
 
     fsevents_logger = logging.getLogger("fsevents")
-    fsevents_logger.setLevel(logging.WARNING)
+    fsevents_logger.setLevel(logging.INFO)
 
     main()
